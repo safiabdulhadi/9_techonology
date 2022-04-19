2a7d5ec761469b6f539547c1aacb5701716cdfa5
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-04-19 02:11:15 +0200

    commit

---
+++ javascript-ES6/Operators.js
@@ -0,0 +1,40 @@
+// Assignment operator
+// let x = 10;
+// let y;
+// y = x ;
+// console.log(y);
+
+// let x = 10;
+// let y = 20;
+
+// x += y ;
+// x -= y ;
+// x *= y ;
+// x /= y ;
+// console.log(x);
+
+// let x = 10;
+
+// x = x + 1;
+// x += 1;
+// x++;
+// x--;
+
+
+// console.log(x ** 2);
+
+
+//  Logical operators
+
+// Logical AND &&
+// let happy = true && true;
+// console.log(happy);
+
+
+const isLoggedIn = true;
+const hasPermissions = true;
+
+if(isLoggedIn && hasPermissions) {
+    // Delete an item
+    console.log('Item deleted sucessfully');
+}
