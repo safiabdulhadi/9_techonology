f731c2e50ec07d57889e36b1706417ef28a11bd2
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-04-20 03:46:16 +0200

    operators js

--- javascript-ES6/Operators.js
+++ javascript-ES6/Operators.js
@@ -48,3 +48,100 @@
 
 
 
+//  const isLoggedIn = true;
+// const hasPermissions = false;
+
+// if(isLoggedIn || hasPermissions) {
+//     // Delete an item
+//     console.log('Item deleted sucessfully');
+// }
+
+
+// 3. Logical not ! 
+
+// // const happy = false ;
+
+// const animal = 'Cat'
+
+// console.log(!animal);
+
+
+//  const isLoggedIn = false;
+// // const hasPermissions = false;s
+
+// if(!isLoggedIn) {
+//     // Delete an item
+//     console.log('redirect to login page');
+// }
+
+
+
+// Conditional operators (Ternary operators)
+
+// const userRole = 'admin';
+
+// if(userRole === 'admin') {
+//     console.log('Your are an admin');
+// } else {
+
+//     console.log('Your are not an admin');
+
+// }
+
+
+// const userRole = 'admin';
+
+// // Condition ? '' : '' ;
+// userRole === 'admin' ? console.log('Your are an admin') : console.log('Your are not an admin');
+
+
+
+
+// const userRole = 'admin';
+// userRole === 'admin' ? 'You are an admin':  'Your are not an admin';
+
+// console.log(userRole);
+
+
+// var age = 19;
+// var drive;
+
+// if(age>=20){
+//     drive = 'Yes Drive';
+// }else {
+//     drive = 'NO drive';
+// }
+
+// console.log(drive);
+
+
+// var age = 19;
+// var drive;
+
+// var drive = age>=18 ? 'Yes Drive' : 'No Drive';
+// console.log(drive);
+
+
+// Comparison operators
+
+// Equl (==)
+// Not equal(!=)
+// Strict equal (===)
+// Strict not equal (!==)
+// Greater than (>)
+// Greater than or equel (>=)
+// Less than (<)
+// Less than or equal (<=)
+
+// console.log(4==5);
+// console.log(4!=5);
+// console.log(4===4);
+// console.log(4==='4'); // return false
+// console.log(4=='4'); // return false
+// console.log(6> 5);
+// console.log(3 + 10 *2);
+// console.log((3 + 10) *2);
+// console.log(3 + 10 * 5 - 5);
+// console.log(3 > 2 && 2 > 3); // false
+// console.log(3 > 2 && 2 > 1); // true
+// console.log(4 > 5 > 9);
