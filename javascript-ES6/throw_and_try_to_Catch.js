a92a47015cc410445a661888904318133df292e4
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-04-30 02:51:26 +0200

    throw and try

---
+++ javascript-ES6/throw_and_try_to_Catch.js
@@ -0,0 +1,23 @@
+function myfunction() {
+    let data = document.getElementById('in1').value;
+    // alert('Hello World!');
+    try{
+        if(data == "") {
+            throw "This is Empty";
+        }
+        if(isNaN (data)){
+            throw "This is not number";
+        }
+        if (data > 20 ){
+            throw "this number is too big";
+        }
+        if (data < 10 ){
+            throw "this is too small";
+        }
+        alert('Thank you ');
+    }
+    catch(err){
+
+        document.getElementById('p00').innerHTML = err;
+    }
+}
